70184ef215e3fdd401f0106ce3be32ae1c580a2d
quentinstroobants <quentin.stroobants@gmail.com>
2021-06-14 16:23:58 +0200

    printjob class

--- javaProject/src/com/company/Main.java
+++ javaProject/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+//https://www.jetbrains.com/help/idea/creating-and-running-your-first-java-application.html#run_jar_artifact will be usefull to build the JAR
+
 public class Main {
 
     public static void main(String[] args) {
