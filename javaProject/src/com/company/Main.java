6518c64937057000a2ff8fc6c6d397d4d221a336
quentinstroobants <quentin.stroobants@gmail.com>
2021-06-15 12:25:52 +0200

    AOP server can be reached

--- javaProject/src/com/company/Main.java
+++ javaProject/src/com/company/Main.java
@@ -2,10 +2,15 @@ package com.company;
 
 //https://www.jetbrains.com/help/idea/creating-and-running-your-first-java-application.html#run_jar_artifact will be usefull to build the JAR
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         System.out.println("hello world");
+        Server server = new Server();
+        server.sendGETRequest("http://localhost:8010/supported_template_mimetypes"); // /marco for polo
+        //server.sendPOSTRequest("http://localhost:8010");
     }
 }
--- javaProject/src/com/company/Server.java
+++ javaProject/src/com/company/Server.java
@@ -1,14 +1,24 @@
 package com.company;
 
+import com.sun.tools.internal.ws.wsdl.document.Output;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
 
     int APIKey;
     String loginInfo;
     String url;
+
+    Server(){
+    }
 
     Server(String url){
         url = url;
@@ -18,13 +28,61 @@ public class Server {
         return false;
     }
 
-    void main() throws IOException {
-        String url = "www.example.com";
+    public void sendGETRequest(String urlToJoin) throws IOException {
+        String url = urlToJoin;
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         int responseCode = con.getResponseCode();
         System.out.println("\nSending 'GET' request to URL : " + url);
         System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print in String
+        System.out.println(response.toString());
+
+        //Read JSON response and print
+        JSONObject myResponse = new JSONObject(response.toString());
+        System.out.println("result after Reading JSON Response");
+        System.out.println(myResponse);
+        System.out.println(myResponse.get("docx"));
+    }
+    public void sendPOSTRequest(String urlToJoin) throws IOException {
+        String post_data = "key1=value1&key2=value2";
+        String url = urlToJoin;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+
+        OutputStream outputStream = con.getOutputStream();
+        outputStream.write(post_data.getBytes());
+        outputStream.flush();
+        outputStream.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print in String
+        System.out.println(response.toString());
+
+        //Read JSON response and print
+        JSONObject myResponse = new JSONObject(response.toString());
+        System.out.println("result after Reading JSON Response");
+        System.out.println(myResponse.get("args"));
+        System.out.println(myResponse);
+        //System.out.println("statusCode- "+myResponse.getString("statusCode"));
     }
 }
