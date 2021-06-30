a6e439c63b3e2eec353cd69c24232d6c0cb74e54
quentinstroobants <quentin.stroobants@gmail.com>
2021-06-30 12:30:21 +0200

    Update AreaChart.java

--- javaProject/src/com/company/RenderElements/Charts/Charts/AreaChart.java
+++ javaProject/src/com/company/RenderElements/Charts/Charts/AreaChart.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class AreaChart extends Chart{
     ArrayList<AreaSeries> series = new ArrayList<>();
 
+    /**
      * @return Serie with the data for the chart.
      */
     public ArrayList<AreaSeries> getSeries() {
