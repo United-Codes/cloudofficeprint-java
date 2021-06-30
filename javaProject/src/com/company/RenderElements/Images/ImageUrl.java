04929c911f374236d3b161c5cf66e42358e03824
quentinstroobants <quentin.stroobants@gmail.com>
2021-06-30 15:47:16 +0200

    images done

---
+++ javaProject/src/com/company/RenderElements/Images/ImageUrl.java
@@ -0,0 +1,21 @@
+package com.company.RenderElements.Images;
+
+
+/**
+ * Represents an image to insert in a template with a URL string as source.
+ */
+public class ImageUrl extends Image{
+
+    /**
+     * This object represent an image to insert in the template. The options of the image can be turned on via the setter functions.
+     * The source of this image is a URL string.
+     * @param name Name of this image for the tag.
+     * @param url Url of the image. Thu url of the image is automatically set as targetUrl (url to jump to when the image is clicked).
+     *            If you want to specify another targetUrl you can do this via the setTargetUrl() function.
+     */
+    public ImageUrl(String name, String url){
+        setName(name);
+        setValue(url);
+    }
+
+}
