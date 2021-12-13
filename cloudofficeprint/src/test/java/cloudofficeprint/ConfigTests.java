ad68890a4ec8d6f1495255678083aa6625714d92
ram-arthasoft <ramchandra@arthasoftsolutions.com>
2021-12-13 18:09:20 +0545

    added test method for ipp printer testing

--- cloudofficeprint/src/test/java/cloudofficeprint/ConfigTests.java
+++ cloudofficeprint/src/test/java/cloudofficeprint/ConfigTests.java
@@ -49,7 +49,7 @@ public class ConfigTests {
 
         Output output = new Output("pdf", "raw", "libreoffice", null, null, pdfOptions, null);
 
-        String correct = "{'output_type': 'pdf', 'output_encoding': 'raw', 'output_converter': 'libreoffice', 'output_read_password': 'test_pw', 'output_watermark': 'test_watermark', 'output_page_width': '500', 'output_page_height': '500', 'output_even_page': True, 'output_merge_making_even': False, 'output_modify_password': 'test_modify_password', 'output_password_protection_flag': 0, 'lock_form': True, 'output_copies': 3, 'page_margin': {'top': 5, 'bottom': 5, 'left': 5, 'right': 5}, 'output_page_format': 'test_page_format', 'output_merge': False, 'output_sign_certificate': 'test_sign_certificate', 'identify_form_fields': True, 'output_split': False}";
+        String correct = "{'output_type': 'pdf', 'output_encoding': 'raw', 'output_converter': 'libreoffice', 'output_read_password': 'test_pw', 'output_watermark': 'test_watermark','output_watermark_color':'blue','output_watermark_font':'Aerial','output_watermark_opacity': 60, 'output_watermark_size':30, 'output_page_width': '500', 'output_page_height': '500', 'output_even_page': True, 'output_merge_making_even': False, 'output_modify_password': 'test_modify_password', 'output_password_protection_flag': 0, 'lock_form': True, 'output_copies': 3, 'page_margin': {'top': 5, 'bottom': 5, 'left': 5, 'right': 5}, 'output_page_format': 'test_page_format', 'output_merge': False, 'output_sign_certificate': 'test_sign_certificate','output_sign_certificate_password':'Base64 certificate with password', 'identify_form_fields': True, 'output_split': False,'output_remove_last_page':true}";
         // System.out.println(output.getJSON());
         JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
         // System.out.println(jsonCorrect);
@@ -157,10 +157,14 @@ public class ConfigTests {
     @Test
     public void testPrinter() {
         Printer printer = new Printer("http://10.0.14.223:631/", "1.1", "your name", "Cloud Office Print",true);
+        Printer printer1 = new Printer("http://10.0.14.223:631/", "1.1", "your name", "Cloud Office Print");
         String correct = " {  'location': 'http://10.0.14.223:631/', 'version': '1.1','requester': 'your name', 'job_name': 'Cloud Office Print','return_output':true }";
-        // System.out.println(printer.getJSON());
+        String correct1 = " {  'location': 'http://10.0.14.223:631/', 'version': '1.1','requester': 'your name', 'job_name': 'Cloud Office Print','return_output':False }";
+//         System.out.println(printer1.getJSON());
         JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
+        JsonObject jsonCorrect1 = JsonParser.parseString(correct1).getAsJsonObject();
         // System.out.println(jsonCorrect);
         assertEquals(jsonCorrect, printer.getJSON());
+        assertEquals(jsonCorrect1,printer1.getJSON());
     }
 }
