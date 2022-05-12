bc99ee027bc55d635ddc2d7b24e3813895932d9e
SibrenJacobs <sibren.jacobs@student.ucll.be>
2022-05-12 16:17:04 +0200

    added documentation to AutoLink

--- cloudofficeprint/src/main/java/com/cloudofficeprint/RenderElements/AutoLink.java
+++ cloudofficeprint/src/main/java/com/cloudofficeprint/RenderElements/AutoLink.java
@@ -5,8 +5,16 @@ import com.google.common.collect.ImmutableSet;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Class for an auto link element. Only supported in Word, Excel and PowerPoint.
+ */
 public class AutoLink extends Property {
 
+    /**
+     * Element for an auto link tag.
+     * @param name Name of this auto link for the tag.
+     * @param text Text to replace the tag and to automatically detect links in it.
+     */
     public AutoLink(String name, String text){
         super(name,text);
     }
