705a3797befc67baf1c5a443c2c791ce631b3186
prabinbessie <bhandari16963@icloud.com>
2025-04-23 13:15:12 +0545

    Add PDF include feature and test

--- cloudofficeprint/src/test/java/cloudofficeprint/RenderElementsTests.java
+++ cloudofficeprint/src/test/java/cloudofficeprint/RenderElementsTests.java
@@ -24,15 +24,6 @@ public class RenderElementsTests {
         assertEquals(jsonCorrect, property.getJSON());
     }
 
-    @Test
-    public void Html() {
-        HTML html = new HTML("name", "<!DOCTYPE html> <html> <body> <h2>An ordered HTML list</h2> <ol> <li value=\\\"2\\\">Coffee</li> <li>Tea</li> <li>Milk</li> </ol> </body> </html>", "CustomTableAOP", "1", "2", false, true, false);
-        String correct = "{'name': '<!DOCTYPE html> <html> <body> <h2>An ordered HTML list</h2> <ol> <li value=\\\\\\\"2\\\\\\\">Coffee</li> <li>Tea</li> <li>Milk</li> </ol> </body> </html>', 'name_custom_table_style': 'CustomTableAOP', 'name_unordered_list_style': '1', 'name_ordered_list_style': '2', 'name_use_tag_style': false, 'name_ignore_cell_margin': true, 'name_ignore_empty_p': false}";
-        // System.out.println(property.getJSON());
-        JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
-        // System.out.println(jsonCorrect);
-        assertEquals(jsonCorrect, html.getJSON());
-    }
 
     @Test
     public void cellStylePropertyDocx() {
@@ -210,6 +201,13 @@ public class RenderElementsTests {
         JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
         assertEquals(jsonCorrect, insert.getJSON());
     }
+    @Test
+    public void include() {
+        PdfInclude include = new PdfInclude("doc", "Base64 encoded file");
+        String correct = "{\"doc\":\"Base64 encoded file\"}";
+        JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
+        assertEquals(jsonCorrect, include.getJSON());
+    }
 
     @Test
     public void remove() {
@@ -334,4 +332,6 @@ public class RenderElementsTests {
         JsonObject jsonCorrect = JsonParser.parseString(correct).getAsJsonObject();
         assertEquals(jsonCorrect, validate.getJSON());
     }
+
+
 }
